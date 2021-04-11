2f33b86d6f823f5d8879a38a59125964090a6351
William Werst <werstiv@gmail.com>
2021-04-11 05:43:35 -0700

    Add scanning of a bunch of pids

--- demo.py
+++ demo.py
@@ -6,9 +6,22 @@ def spin(connection):
     cmd = obd.commands.ENGINE_LOAD # select an OBD command (sensor)
     response = connection.query(cmd) # send the command, and parse the response
     print(f'Engine Load: {response.value:.3f}') # returns unit-bearing values thanks to Pint
-    cmd = obd.commands.PIDS_A
+    cmd = obd.commands.FUEL_STATUS
     response = connection.query(cmd)
-    print(f'PIDS_A: {response.value}')
+    print(f'FUEL_STATUS: {response.value}')
+    cmd = obd.commands.FUEL_LEVEL
+    response = connection.query(cmd)
+    print(f'FUEL_LEVEL: {response.value}')
+
+
+def spin_all(connection, valid_pids):
+    print_strs = []
+    for pid_num in valid_pids:
+        cmd = obd.commands[1][pid_num]
+        response = connection.query(cmd)
+        print_strs.append(f'{cmd.name}: {response.value}')
+    for print_line in print_strs:
+        print(print_line)
 
 
 def main():
@@ -17,30 +30,54 @@ def main():
     try:
         connection = obd.OBD('/dev/rfcomm1') # auto-connects to USB or RF port
 
+        valid_pids = []
         cmd = obd.commands.PIDS_A
         response = connection.query(cmd)
         print(f'PIDS_A: {response.value}')
         for i in range(32):
-            print(f'PID {hex(i+1)} Supported: {response.value[i]}')
+            pid_num = i+1
+            if response.value[i] == 1 and i != 31:
+                valid_pids.append(pid_num)
+            print(f'PID {hex(pid_num)} Supported: {response.value[i]}')
 
         cmd = obd.commands.PIDS_B
         response = connection.query(cmd)
         print(f'PIDS_B: {response.value}')
         for i in range(32):
-            print(f'PID {hex(32+i+1)} Supported: {response.value[i]}')
+            pid_num = 32+i+1
+            if response.value[i] == 1 and i != 31:
+                valid_pids.append(pid_num)
+            print(f'PID {hex(pid_num)} Supported: {response.value[i]}')
 
         cmd = obd.commands.PIDS_C
         response = connection.query(cmd)
         print(f'PIDS_C: {response.value}')
         for i in range(32):
-            print(f'PID {hex(64+i+1)} Supported: {response.value[i]}')
+            pid_num = 64+i+1
+            if response.value[i] == 1 and i != 31:
+                valid_pids.append(pid_num)
+            print(f'PID {hex(pid_num)} Supported: {response.value[i]}')
+
+        blacklist_pids = [
+            obd.commands.O2_SENSORS.pid,
+            obd.commands.OBD_COMPLIANCE.pid,
+            obd.commands.STATUS.pid,
+            obd.commands.STATUS_DRIVE_CYCLE.pid,
+        ]
+
+        scan_pids = [p for p in valid_pids if p not in blacklist_pids]
 
         start_time = time.time()
-        LOOP_TIME = 1.0  # seconds
+        LOOP_TIME_GOAL = 2.0  # seconds
 
         while True:
-            spin(connection)
-            time.sleep(max(0, LOOP_TIME + start_time - time.time()))
+            spin_all(connection, scan_pids)
+            spin_time = time.time() - start_time
+            print(f'Loop took {spin_time:.3f} seconds')
+            print()
+            print()
+            print()
+            time.sleep(max(0, LOOP_TIME_GOAL - spin_time))
             start_time = time.time()
     finally:
         connection.close()
